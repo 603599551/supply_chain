6f8c8f71bb2318c6692da175264b16c2b8bbf6a7
zhanjinqi <603599551@qq.com>
2018-11-06 19:39:32 +0800

    该类实现以下功能： 1.单个地址信息的增改查 2.查询地址表中所有城市/省份(不含重复)

--- src/com/works/pc/sys/controllers/AddressCtrl.java
+++ src/com/works/pc/sys/controllers/AddressCtrl.java
@@ -2,8 +2,19 @@ package com.works.pc.sys.controllers;
 
 import com.common.controllers.BaseCtrl;
 import com.jfinal.plugin.activerecord.Record;
+import com.utils.JsonHashMap;
+import com.utils.StringUtil;
 import com.works.pc.sys.services.AddressService;
+import org.apache.commons.lang.StringUtils;
 
+import java.util.List;
+
+/**
+ * 该类实现以下功能：
+ * 1.地址表信息的增删改查
+ * 2.查询地址表中所有城市/省份(不含重复)
+ * @author CaryZ
+ */
 public class AddressCtrl extends BaseCtrl<AddressService> {
 
     public AddressCtrl() {
@@ -28,5 +39,18 @@ public class AddressCtrl extends BaseCtrl<AddressService> {
     @Override
     public void createRecordBeforeSelect(Record record) {
 
+    }
+
+    /**
+     * 该方法实现查询地址表中所有城市或者省份，去除重复功能
+     * 可应用在所有门店、仓库所在城市或者省份的下拉列表
+     * @author CaryZ
+     * @date 2018-11-04
+     */
+    public void queryCityOrProvince(){
+        JsonHashMap jhm=new JsonHashMap();
+        String keyword=getPara("keyword");
+        jhm.putSuccess(service.queryCityOrProvince(keyword));
+        renderJson(jhm);
     }
 }
--- src/com/works/pc/sys/services/AddressService.java
+++ src/com/works/pc/sys/services/AddressService.java
@@ -2,11 +2,24 @@ package com.works.pc.sys.services;
 
 import com.common.service.BaseService;
 import com.bean.TableBean;
+import com.exception.PcException;
+import com.jfinal.aop.Before;
+import com.jfinal.plugin.activerecord.Db;
 import com.jfinal.plugin.activerecord.Page;
 import com.jfinal.plugin.activerecord.Record;
+import com.jfinal.plugin.activerecord.tx.Tx;
 
 import java.util.List;
 
+
+/**
+ * 该类实现以下功能：
+ * 1.单个地址信息的增改查
+ * 2.查询地址表中所有城市/省份(不含重复)
+ * @author CaryZ
+ */
+
+@Before(Tx.class)
 public class AddressService extends BaseService {
 
     private static String[] columnNameArr = {"id","city","province","address","state"};
@@ -26,5 +39,19 @@ public class AddressService extends BaseService {
     public Page<Record> queryBeforeReturn(Page<Record> page) {
         return page;
      }
+
+
+    /**
+     * 该方法实现查询地址表中所有城市或者省份，去除重复功能
+     * 可应用在所有门店、仓库所在城市或者省份的下拉列表
+     * @author CaryZ
+     * @date 2018-11-04
+     * @param keyword 查询依据的字段名 city/province
+     * @return 返回集合 所有城市或者省份 去除重复
+     */
+    public List<Record> queryCityOrProvince(String keyword){
+            List<Record> list= Db.find("SELECT DISTINCT " +keyword+ " AS name,"+keyword+" AS value FROM s_address ORDER BY "+keyword+" ASC");
+            return list;
+    }
 
 }
