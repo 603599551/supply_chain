aa39a5722944ba95f5f5197e95ee50e2f9bee6a9
zhanjinqi <603599551@qq.com>
2018-11-07 21:29:43 +0800

    * 该类实现以下功能：  * 1.单个门店信息的增改查  * 2.分页查询门店列表

--- src/com/works/pc/store/controllers/StoreCtrl.java
+++ src/com/works/pc/store/controllers/StoreCtrl.java
@@ -1,32 +1,80 @@
 package com.works.pc.store.controllers;
 
 import com.common.controllers.BaseCtrl;
+import com.constants.DictionaryConstants;
 import com.jfinal.plugin.activerecord.Record;
+import com.utils.UserSessionUtil;
 import com.works.pc.store.services.StoreService;
+import org.apache.commons.lang.StringUtils;
 
+import static com.constants.DictionaryConstants.STATE;
+
+/**
+ * 该类实现以下功能：
+ * 1.单个门店信息的增改查
+ * 2.分页查询门店列表
+ * @author CaryZ
+ * @date 2018-11-07
+ */
 public class StoreCtrl extends BaseCtrl<StoreService> {
+
+    UserSessionUtil usu=new UserSessionUtil(getRequest());
+
+    private static final String FIELD_PHONE="phone";
+    private static final String FIELD_PINYIN="pinyin";
+    private static final String FIELD_NAME="name";
+    private static final String FIELD_ADDRESS="address";
+    private static final String FIELD_STATE="state";
+    private static final String FIELD_SORT="sort";
 
     public StoreCtrl() {
         super(StoreService.class);
     }
 
+    /**
+     * 根据目标Record的state字段值(value)读取字典表得到state_text(name)
+     * @author CaryZ
+     * @date 2018-11-07
+     * @param record 目标record
+     */
     @Override
     public void handleRecord(Record record) {
-
+        String state=record.getStr("state");
+        if (StringUtils.isEmpty(state)){
+            return;
+        }
+        Record r= DictionaryConstants.DICT_RECORD_MAP.get(STATE).get(state);
+        record.set("state_text",r.getStr("name"));
     }
 
     @Override
     public void handleAddRecord(Record record) {
-
+        record.set("create_id",usu.getSysUserId());
     }
 
     @Override
     public void handleUpdateRecord(Record record) {
-
+        record.set("create_id",usu.getSysUserId());
     }
 
+    /**
+     * list/query方法支持如下查询：
+     * 模糊查询：门店名称、名称拼音、联系电话、详细地址（包含省市）
+     * 完全匹配查询：门店状态、所在城市
+     * list/query方法排序依据（有先后顺序）：
+     * 门店状态DESC、排序ASC
+     * @author CaryZ
+     * @date 2018-11-07
+     * @param record 查询条件
+     */
     @Override
     public void createRecordBeforeSelect(Record record) {
-
+        String keyword=record.getStr("keyword");
+        if (StringUtils.isNotEmpty(keyword)){
+            String []keywords=new String[]{keyword,keyword,keyword,keyword};
+            record.set("$all$and_"+FIELD_PHONE+"$like$or_"+FIELD_NAME+"$like$or_"+FIELD_PINYIN+"$like$or_"+FIELD_ADDRESS+"$like$or",keywords);
+            record.remove("keyword");
+        }
+        record.set("$sort"," ORDER BY "+FIELD_STATE+" DESC,"+FIELD_SORT+" ASC");
     }
 }
--- src/com/works/pc/store/services/StoreService.java
+++ src/com/works/pc/store/services/StoreService.java
@@ -2,19 +2,34 @@ package com.works.pc.store.services;
 
 import com.common.service.BaseService;
 import com.bean.TableBean;
+import com.exception.PcException;
 import com.jfinal.plugin.activerecord.Page;
 import com.jfinal.plugin.activerecord.Record;
+import com.utils.DateUtil;
+import com.utils.HanyuPinyinHelper;
+import com.utils.UserSessionUtil;
+import com.works.pc.sys.services.AddressService;
 
+import javax.servlet.http.HttpServletRequest;
 import java.util.List;
 
+/**
+ * 该类实现以下功能：
+ * 1.单个门店信息的增改查
+ * 2.分页查询门店列表
+ * @author CaryZ
+ * @date 2018-11-07
+ */
 public class StoreService extends BaseService {
+    AddressService addressService=super.enhance(AddressService.class);
 
-    private static String[] columnNameArr = {"id","create_id","address_id","name","city","address","phone","sort","updatedate","state","color","remark"};
-    private static String[] columnTypeArr = {"VARCHAR","VARCHAR","VARCHAR","VARCHAR","VARCHAR","VARCHAR","VARCHAR","INT","VARCHAR","INT","VARCHAR","VARCHAR"};
-    private static String[] columnCommentArr = {"","","","","","","","","","","",""};
+    private static final String TABLENAME="s_store";
+    private static String[] columnNameArr = {"id","create_id","address_id","name","pinyin","city","address","phone","sort","updatedate","state","color","remark"};
+    private static String[] columnTypeArr = {"VARCHAR","VARCHAR","VARCHAR","VARCHAR","VARCHAR","VARCHAR","VARCHAR","VARCHAR","INT","VARCHAR","INT","VARCHAR","VARCHAR"};
+    private static String[] columnCommentArr = {"","","","","","","","","","","","",""};
 
     public StoreService() {
-        super("s_store", new TableBean("s_store", columnNameArr, columnTypeArr, columnCommentArr));
+        super(TABLENAME, new TableBean(TABLENAME, columnNameArr, columnTypeArr, columnCommentArr));
     }
 
     @Override
@@ -27,4 +42,62 @@ public class StoreService extends BaseService {
         return page;
      }
 
+    /**
+     * 该方法逻辑如下：
+     * 将门店信息的省,市,详细地址（不包含省市）合起来存到address中
+     * 根据address详细地址（包含省市）在地址表查询是否已存在地址信息
+     * 若存在，则不在地址表中添加，若不存在，则在地址表中添加
+     * 将地址id填到门店的address_id,最后再新增门店信息
+     * @author CaryZ
+     * @date 2018-11-07
+     * @param record 新增的门店信息
+     * @return 新增成功/失败 返回record的id/null
+     * @throws PcException
+     */
+    @Override
+    public String add(Record record) throws PcException {
+        record.set("state",1);
+        record.set("updatedate", DateUtil.GetDateTime());
+        record.set("pinyin", HanyuPinyinHelper.getPinyinString(record.getStr("name")));
+        record.set("address",record.getStr("province")+record.getStr("city")+record.getStr("address"));
+        if (!addressService.isExist(record)){
+            return null;
+        }
+        record.remove("province");
+        return super.add(record);
+    }
+
+    /**
+     * 通过id联合查询s_address、s_store表得到门店信息
+     * 联合查询的目的是将门店所在的省份province查询出来
+     * 将包含省市的address中的省市信息去掉
+     * @author CaryZ
+     * @date 2018-11-07
+     * @param id 记录id
+     * @return 返回一条门店记录
+     */
+    @Override
+    public Record findById(String id){
+        return addressService.queryMessage(id,TABLENAME,true);
+    }
+
+
+    /**
+     * 通过id修改门店信息，并更新地址表的记录信息
+     * @author CaryZ
+     * @date 2018-11-07
+     * @param record 要修改成的数据
+     * @return 修改成功返回true，否则修改false
+     * @throws PcException
+     */
+    @Override
+    public boolean updateById(Record record) throws PcException{
+        String id=record.getStr("id");
+        Record oldStore=super.findById(id);
+        if (!addressService.updateMessage(record,oldStore,true)){
+            return false;
+        };
+        record.set("updatedate",DateUtil.GetDateTime());
+        return super.updateById(record);
+    }
 }
