3a1da9c7f3137666077da487473d0a09583401c9
wangze <w@1.com>
2018-11-10 14:57:28 +0800

    添加material_id字段

--- src/com/works/pc/warehourse/services/WarehouseStockService.java
+++ src/com/works/pc/warehourse/services/WarehouseStockService.java
@@ -9,9 +9,9 @@ import java.util.List;
 
 public class WarehouseStockService extends BaseService {
 
-    private static String[] columnNameArr = {"id","user_id","warehourse_id","state","quantity","batch_num","material_data","sort"};
-    private static String[] columnTypeArr = {"VARCHAR","VARCHAR","VARCHAR","INT","VARCHAR","VARCHAR","VARCHAR","INT"};
-    private static String[] columnCommentArr = {"","","","","","","",""};
+    private static String[] columnNameArr = {"id","user_id","warehourse_id","state","quantity","batch_num","material_data","sort","material_id"};
+    private static String[] columnTypeArr = {"VARCHAR","VARCHAR","VARCHAR","INT","VARCHAR","VARCHAR","VARCHAR","INT","VARCHAR"};
+    private static String[] columnCommentArr = {"","","","","","","","",""};
 
     public WarehouseStockService() {
         super("s_warehouse_stock", new TableBean("s_warehouse_stock", columnNameArr, columnTypeArr, columnCommentArr));
