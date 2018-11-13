206ec8403aa7c47ab84727e1820d8b248d318362
wangze <w@1.com>
2018-11-13 10:01:07 +0800

    添加商品类型字典值

---
+++ sql/2018111301_wz_dictionary_insert.sql
@@ -0,0 +1,3 @@
+INSERT INTO `s_dictionary` VALUES ('10000', '商品类型', '0', 'product_type', NULL, 10000);
+INSERT INTO `s_dictionary` VALUES ('10100', '预制商品', '10000', 'prefabricated_goods', NULL, 10100);
+INSERT INTO `s_dictionary` VALUES ('10200', '现制商品', '10000', 'present_commodity', NULL, 10200);
