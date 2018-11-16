ff05c5511b70d673c090f8571c75e82904c4bd6c
zhanjinqi <603599551@qq.com>
2018-11-16 21:47:57 +0800

    补上 关闭和完成两个状态

--- sql/2018111601_zjq_dictionary_insert.sql
+++ sql/2018111601_zjq_dictionary_insert.sql
@@ -3,3 +3,5 @@ INSERT INTO `s_dictionary` VALUES ('7010', '物流清点', '7000', 'logistics_cl
 INSERT INTO `s_dictionary` VALUES ('7020', '采购审核', '7000', 'purchase_audit', NULL, 7020);
 INSERT INTO `s_dictionary` VALUES ('7030', '财务确认', '7000', 'finance_confirm', NULL, 7030);
 INSERT INTO `s_dictionary` VALUES ('7040', '物流发货', '7000', 'logistics_delivery', NULL, 7040);
+INSERT INTO `s_dictionary` VALUES ('7050', '退货关闭', '7000', 'return_shutdown', NULL, 7050);
+INSERT INTO `s_dictionary` VALUES ('7060', '退货完成', '7000', 'return_finish', NULL, 7060);
