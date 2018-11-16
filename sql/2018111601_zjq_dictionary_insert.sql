94f335665fde22b38a068be4aa117a2b23e6ac5e
zhanjinqi <603599551@qq.com>
2018-11-16 21:43:15 +0800

    字典值新增采购退货单状态

---
+++ sql/2018111601_zjq_dictionary_insert.sql
@@ -0,0 +1,5 @@
+INSERT INTO `s_dictionary` VALUES ('7000', '采购退货单状态', '0', 'purchase_return_type', NULL, 7000);
+INSERT INTO `s_dictionary` VALUES ('7010', '物流清点', '7000', 'logistics_clearance', NULL, 7010);
+INSERT INTO `s_dictionary` VALUES ('7020', '采购审核', '7000', 'purchase_audit', NULL, 7020);
+INSERT INTO `s_dictionary` VALUES ('7030', '财务确认', '7000', 'finance_confirm', NULL, 7030);
+INSERT INTO `s_dictionary` VALUES ('7040', '物流发货', '7000', 'logistics_delivery', NULL, 7040);
