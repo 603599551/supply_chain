996f6f3c6e517d781f46760b8055bfc166c502ab
zhanjinqi <603599551@qq.com>
2018-11-24 19:18:50 +0800

    实现门店退货流程(提交门店退货单，等到小程序时再写，整个流程待测试)

--- src/com/works/pc/order/controllers/OrderReturnCtrl.java
+++ src/com/works/pc/order/controllers/OrderReturnCtrl.java
@@ -1,10 +1,32 @@
 package com.works.pc.order.controllers;
 
+import com.alibaba.fastjson.JSONObject;
 import com.common.controllers.BaseCtrl;
+import com.exception.PcException;
 import com.jfinal.plugin.activerecord.Record;
+import com.utils.BeanUtils;
+import com.utils.JsonHashMap;
+import com.utils.UserSessionUtil;
 import com.works.pc.order.services.OrderReturnService;
+import org.apache.commons.lang.StringUtils;
 
+/**
+ * @author CaryZ
+ * @date 2018-11-23
+ * 实现门店退货流程
+ * 1.小程序提交退货单
+ * 2.pc端店长查看退货单（图片传全路径）
+ * 3.PC端店长可以撤销退货单，前提在物流没有接收退货单基础上
+ * 4.物流接收退货单，可以仿照原系统。修改订单状态，修改门店库存。
+ * 5.物流退回退货单，修改订单状态，修改门店库存。
+ * 6.物流完成退货单，修改订单状态、修改物流仓库库存。
+ */
 public class OrderReturnCtrl extends BaseCtrl<OrderReturnService> {
+
+    UserSessionUtil usu=new UserSessionUtil(getRequest());
+
+    private static final String FIELD_NUM="num";
+    private static final String FIELD_CREATEDATE="create_date";
 
     public OrderReturnCtrl() {
         super(OrderReturnService.class);
@@ -15,6 +37,10 @@ public class OrderReturnCtrl extends BaseCtrl<OrderReturnService> {
 
     }
 
+    /**
+     * 新增退货单，放到小程序里，这里不做
+     * @param record
+     */
     @Override
     public void handleAddRecord(Record record) {
 
@@ -25,8 +51,111 @@ public class OrderReturnCtrl extends BaseCtrl<OrderReturnService> {
 
     }
 
+    /**
+     * 完全匹配查询：退货单状态
+     * 模糊查询：退货单号
+     * @param record 查询条件
+     */
     @Override
     public void createRecordBeforeSelect(Record record) {
+        String keyword=record.getStr("keyword");
+        if (StringUtils.isNotEmpty(keyword)){
+            String []keywords=new String[]{keyword};
+            record.set("$all$and#"+FIELD_NUM+"$like$or",keywords);
+            record.remove("keyword");
+        }
+        record.set("$sort"," ORDER BY "+FIELD_CREATEDATE+" DESC");
+    }
 
+    /**
+     * 物流接收退货单
+     * 退货单状态：未接收->已接收
+     * 每个原料信息多了warehouse_stock_id，修改订单状态，减少门店库存。
+     */
+    public void acceptReturnItems(){
+        JsonHashMap jhm = new JsonHashMap();
+        JSONObject json = getJson(getRequest());
+        Record record = BeanUtils.jsonToRecord(json);
+        try {
+            boolean flag = service.acceptReturnItems(record);
+            if(flag){
+                jhm.putMessage("接收成功！");
+            }else{
+                jhm.putFail("接收失败！");
+            }
+        } catch (PcException e) {
+            e.printStackTrace();
+            jhm.putError(e.getMsg());
+        }
+        renderJson(jhm);
+    }
+
+    /**
+     * 物流完成退货单
+     * 退货单状态：已接收->已完成
+     * 修改订单状态、修改物流仓库库存。
+     */
+    public void finishOrder(){
+        JsonHashMap jhm = new JsonHashMap();
+        JSONObject json = getJson(getRequest());
+        Record record = BeanUtils.jsonToRecord(json);
+        try {
+            boolean flag = service.finishOrder(record);
+            if(flag){
+                jhm.putMessage("完成成功！");
+            }else{
+                jhm.putFail("完成失败！");
+            }
+        } catch (PcException e) {
+            e.printStackTrace();
+            jhm.putError(e.getMsg());
+        }
+        renderJson(jhm);
+    }
+
+    /**
+     * 店长撤销退货单
+     * 退货单状态：已撤销
+     * 修改订单状态
+     */
+    public void revokeOrder(){
+        JsonHashMap jhm = new JsonHashMap();
+        JSONObject json = getJson(getRequest());
+        Record record = BeanUtils.jsonToRecord(json);
+        try {
+            boolean flag = service.revokeOrder(record);
+            if(flag){
+                jhm.putMessage("撤销成功！");
+            }else{
+                jhm.putFail("撤销失败！");
+            }
+        } catch (PcException e) {
+            e.printStackTrace();
+            jhm.putError(e.getMsg());
+        }
+        renderJson(jhm);
+    }
+
+    /**
+     * 物流退回退货单
+     * 退货单状态：已退回
+     * 修改订单状态，增加门店库存。
+     */
+    public void returnOrder(){
+        JsonHashMap jhm = new JsonHashMap();
+        JSONObject json = getJson(getRequest());
+        Record record = BeanUtils.jsonToRecord(json);
+        try {
+            boolean flag = service.returnOrder(record);
+            if(flag){
+                jhm.putMessage("退回成功！");
+            }else{
+                jhm.putFail("退回失败！");
+            }
+        } catch (PcException e) {
+            e.printStackTrace();
+            jhm.putError(e.getMsg());
+        }
+        renderJson(jhm);
     }
 }
--- src/com/works/pc/order/services/OrderReturnService.java
+++ src/com/works/pc/order/services/OrderReturnService.java
@@ -1,20 +1,45 @@
 package com.works.pc.order.services;
 
-import com.common.service.BaseService;
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONArray;
+import com.alibaba.fastjson.JSONObject;
 import com.bean.TableBean;
+import com.common.service.BaseService;
+import com.exception.PcException;
+import com.jfinal.aop.Before;
 import com.jfinal.plugin.activerecord.Page;
 import com.jfinal.plugin.activerecord.Record;
+import com.jfinal.plugin.activerecord.tx.Tx;
+import com.utils.DateUtil;
+import com.works.pc.store.services.StoreStockService;
+import com.works.pc.warehourse.services.WarehouseStockService;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
+/**
+ * @author CaryZ
+ * @date 2018-11-23
+ * 实现门店退货流程
+ * 1.小程序提交退货单
+ * 2.pc端店长查看退货单（图片传全路径）
+ * 3.PC端店长可以撤销退货单，前提在物流没有接收退货单基础上
+ * 4.物流接收退货单，可以仿照原系统。修改订单状态，修改门店库存。
+ * 5.物流退回退货单，修改订单状态，修改门店库存。
+ * 6.物流完成退货单，修改订单状态、修改物流仓库库存。
+ */
+@Before(Tx.class)
 public class OrderReturnService extends BaseService {
 
+    private static final String TABLENAME="s_order_return";
+    private static String[] orderReturnState={"unaccepted","accepted","finished","revoked","returned"};
     private static String[] columnNameArr = {"id","store_id","num","order_date","arrive_date","order_item","store_color","order_state","create_id","create_date","logistics_id","logistics_date","order_type","close_date","close_reason","close_id","city","remark","image","return_reason"};
     private static String[] columnTypeArr = {"VARCHAR","VARCHAR","VARCHAR","VARCHAR","VARCHAR","VARCHAR","VARCHAR","VARCHAR","VARCHAR","VARCHAR","VARCHAR","VARCHAR","VARCHAR","VARCHAR","VARCHAR","VARCHAR","VARCHAR","VARCHAR","VARCHAR","VARCHAR"};
     private static String[] columnCommentArr = {"","","","","","","","","","","","","","","","","","","",""};
 
     public OrderReturnService() {
-        super("s_order_return", new TableBean("s_order_return", columnNameArr, columnTypeArr, columnCommentArr));
+        super(TABLENAME, new TableBean(TABLENAME, columnNameArr, columnTypeArr, columnCommentArr));
     }
 
     @Override
@@ -24,7 +49,105 @@ public class OrderReturnService extends BaseService {
 
     @Override
     public Page<Record> queryBeforeReturn(Page<Record> page) {
+        List<Record> list=page.getList();
+        for (Record r:list){
+            r.set("order_item", JSONObject.parseObject(r.getStr("order_item")));
+        }
         return page;
      }
+
+    /**
+     * 1.将order_item的格式处理一下
+     * 2.加上当前日期
+     * @param record 要修改成的数据
+     * @return
+     */
+     @Override
+     public boolean updateById(Record record) throws PcException{
+         String orderItems=record.getStr("order_item");
+         JSONArray jsonArray=JSONArray.parseArray(orderItems);
+         Map<String,JSONArray> map=new HashMap(1);
+         map.put("item",jsonArray);
+         record.set("order_item",JSON.toJSONString(map));
+         record.set("logistics_date", DateUtil.GetDateTime());
+         return super.updateById(record);
+     }
+
+    /**
+     * 物流接收退货单
+     * 退货单状态：未接收->已接收
+     * 接收后每个原料信息多了warehouse_stock_id，修改订单状态，减少门店库存。
+     * @param record 退货单信息（order_item从门店库存记录里获取，每个元素组成：material_data+仓库库存记录warehouse_stock_id+门店库存记录store_stock_id+现有数量quantity（出库单位）+要退的数量current_quantity（出库单位））
+     * @return
+     */
+    public boolean acceptReturnItems(Record record) throws PcException{
+        StoreStockService storeStockService=enhance(StoreStockService.class);
+        String orderItems=record.getStr("order_item");
+        JSONArray jsonArray=JSONArray.parseArray(orderItems);
+        Map<String,JSONArray> map=new HashMap(1);
+        map.put("item",jsonArray);
+        record.set("order_item",JSON.toJSONString(map));
+        record.set("order_state",orderReturnState[1]);
+        record.set("logistics_date", DateUtil.GetDateTime());
+        if (!super.updateById(record)){
+            return false;
+        }
+        return storeStockService.batchUpdate(jsonArray,false);
+    }
+
+    /**
+     * 物流完成退货单
+     * 退货单状态：已接收->已完成
+     * 修改订单状态、修改物流仓库库存。
+     * @param record
+     * @return
+     */
+    public boolean finishOrder(Record record) throws PcException{
+        WarehouseStockService warehouseStockService=enhance(WarehouseStockService.class);
+        record.set("arrive_date",DateUtil.GetDate());
+        record.set("order_state",orderReturnState[2]);
+        if (!this.updateById(record)){
+            return false;
+        }
+        return warehouseStockService.updateStockAfterReturn(record.getStr("order_item"),"warehouse_stock_id",true);
+    }
+
+    /**
+     * 店长撤销退货单
+     * 退货单状态：已撤销
+     * 修改订单状态
+     * @param record
+     * @return
+     * @throws PcException
+     */
+    public boolean revokeOrder(Record record) throws PcException{
+        record.set("arrive_date",DateUtil.GetDate());
+        record.set("order_state",orderReturnState[3]);
+        return this.updateById(record);
+    }
+
+    /**
+     * 物流退回退货单
+     * 退货单状态：已退回
+     * 修改订单状态，增加门店库存。
+     * @param record
+     * @return
+     * @throws PcException
+     */
+    public boolean returnOrder(Record record) throws PcException{
+        StoreStockService storeStockService=enhance(StoreStockService.class);
+        String orderItems=record.getStr("order_item");
+        JSONArray jsonArray=JSONArray.parseArray(orderItems);
+        Map<String,JSONArray> map=new HashMap(1);
+        map.put("item",jsonArray);
+        record.set("arrive_date",DateUtil.GetDate());
+        record.set("order_item",JSON.toJSONString(map));
+        record.set("order_state",orderReturnState[4]);
+        record.set("logistics_date", DateUtil.GetDateTime());
+        if (!super.updateById(record)){
+            return false;
+        }
+        return storeStockService.batchUpdate(jsonArray,true);
+    }
 
 }
